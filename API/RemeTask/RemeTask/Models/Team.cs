8387f870ede02e84b5a32501e8874b93d319daf7
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-09-24 00:03:50 +0300

    Database seeding

--- API/RemeTask/RemeTask/Models/Team.cs
+++ API/RemeTask/RemeTask/Models/Team.cs
@@ -12,6 +12,7 @@ namespace RemeTask.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public User Owner { get; set; }
+        public int? OwnerId { get; set; }
         public ICollection<UserTeam> Users { get; set; }
     }
 }
