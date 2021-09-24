702411f9b25332d2edf9c6f0c5ebac8867be7fde
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-09-24 20:52:37 +0300

    Create Task DTO's

---
+++ API/RemeTask/RemeTask/Dtos/Task/TaskReadDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemeTask.Dtos.Task
+{
+    public class TaskReadDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public int Priority { get; set; }
+
+        public int TaskGroupId { get; set; }
+    }
+}
