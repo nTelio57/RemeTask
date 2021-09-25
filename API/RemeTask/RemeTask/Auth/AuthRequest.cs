331b05b72f8b5d8b01acdecdcac8b07cc317fff9
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-09-25 22:42:09 +0300

    Create authorization request model and cryptography utilities class

--- API/RemeTask/RemeTask/Auth/AuthRequest.cs
+++ API/RemeTask/RemeTask/Auth/AuthRequest.cs
@@ -7,8 +7,6 @@ namespace RemeTask.Auth
 {
     public class AuthRequest
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
