8437f91cbc79b83765a1adac98c5c8b2d8ecce32
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-09-25 22:41:20 +0300

    Create authorization repository

---
+++ API/RemeTask/RemeTask/Data/IAuthRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RemeTask.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace RemeTask.Data
+{
+    public interface IAuthRepository
+    {
+        Task<bool> SaveChanges();
+        Task<User> GetUserByEmail(string email);
+        Task<string> GenerateToken(string email);
+        Task AddNewUser(User user);
+        Task<User> GetUserByLogin(string email, string password);
+    }
+}
