35aa3b95515af97001eac1d36a0820e25526a3d1
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-09-25 22:40:39 +0300

    Add user DTO's

---
+++ API/RemeTask/RemeTask/Dtos/User/UserUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemeTask.Dtos.User
+{
+    public class UserUpdateDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+    }
+}
