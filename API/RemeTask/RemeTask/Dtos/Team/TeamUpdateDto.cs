f159d5e0d278746bcab512be9cd6f24c6b040e78
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-09-30 01:01:21 +0300

    Create team DTO's

---
+++ API/RemeTask/RemeTask/Dtos/Team/TeamUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemeTask.Dtos.Team
+{
+    public class TeamUpdateDto
+    {
+        public string Name { get; set; }
+    }
+}
