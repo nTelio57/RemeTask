93d56b5ba9f79ebc1dbf379d7d5aed4598b53b55
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-10-17 21:24:57 +0300

    Update Task card design

--- Mobile/remetask/lib/Components/TaskCard.dart
+++ Mobile/remetask/lib/Components/TaskCard.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:remetask/Models/Task.dart';
 import 'package:remetask/Utilities/constants.dart';
 
 class TaskCard extends StatelessWidget {
 
-  final String title, tag, time, timeType;
-  final Color color, fontColor;
-  const TaskCard({
+  String? time, timeType;
+  Color? color, fontColor;
+  Task task;
+  TaskCard({
     Key? key,
-    this.title = 'Very long title',
-    this.tag = 'TAG',
-    this.time = '4',
-    this.timeType = 'days',
-    this.color = kTaskGood,
-    this.fontColor = kTextOnPrimary
+    required this.task
   }) : super(key: key);
 
   final double _borderRadius = 8;
 
   @override
   Widget build(BuildContext context) {
+    var times = timeLeft(task.deadline);
+    time = times[0];
+    timeType = times[1];
+
+    var colors = cardColors(task.deadline.difference(DateTime.now()).inDays);
+    color = colors[0];
+    fontColor = colors[1];
+
     return taskCard();
   }
 
@@ -56,7 +61,7 @@ class TaskCard extends StatelessWidget {
                             padding: const EdgeInsets.only(left: 10, top: 7, right: 2),
                             child: Container(
                               child: Text(
-                                title,
+                                task.title,
                                 style: kTaskCardTitle,
                                 textAlign: TextAlign.left,
                               ),
@@ -72,7 +77,7 @@ class TaskCard extends StatelessWidget {
                               alignment: Alignment.bottomLeft,
                               height: 25,
                               child: Text(
-                                tag,
+                                task.taskGroup!.tag,
                                 style: kTaskCardTag,
                                 textAlign: TextAlign.left,
                               ),
@@ -86,24 +91,35 @@ class TaskCard extends StatelessWidget {
                     flex: 1,
                     child: Container(
                       decoration: BoxDecoration(
-                        color: color,
+                        color: task.isCompleted! ? kTaskGood : color,
                         borderRadius: BorderRadius.only(topRight: Radius.circular(_borderRadius), bottomRight: Radius.circular(_borderRadius)),
                       ),
                       child: Padding(
                         padding: const EdgeInsets.all(4.0),
-                        child: Column(
+                        child: task.isCompleted! ?
+                        Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          child: Icon(
+                            Icons.check,
+                            size: 35,
+                            color: Colors.white,
+                          )
+                        )
+                            :
+                        Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Container(
                               height: 38,
                               child: Text(
-                                time,
+                                time!,
                                 style: kTaskCardTime,
                               ),
                             ),
                             Container(
                               child: Text(
-                                timeType,
+                                timeType!,
                                 style: kTaskCardTimeType,
                               ),
                             ),
@@ -119,5 +135,39 @@ class TaskCard extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  List<Color> cardColors(int daysLeft)
+  {
+    var list = <Color>[kTaskGood, kTextOnPrimary];
+
+    if(daysLeft > 9){//good
+      list[0] = kTaskGood;
+      list[1] = kTextOnPrimary;
+    }else if(daysLeft > 5){//medium
+      list[0] = kTaskMedium;
+      list[1] = kTextOnPrimary;
+    }else if(daysLeft > 2){//bad
+      list[0] = kTaskBad;
+      list[1] = kTextOnPrimary;
+    }else{//very bad
+      list[0] = kTaskVeryBad;
+      list[1] = kTextOnPrimary;
+    }
+    return list;
+  }
+
+  List<String> timeLeft(DateTime deadline)
+  {
+    var list = <String>['',''];
+    var diff =  deadline.difference(DateTime.now());
+    if(diff.inDays > 1){
+      list[0] = diff.inDays.toString();
+      list[1] = 'days';
+    }else{
+      list[0] = diff.inHours.toString();
+      list[1] = 'hours';
+    }
+    return list;
   }
 }
