bf8e590690f3f79028cf6600a03066b6142fb7bb
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-11 17:52:26 +0200

    Add auth constants classes

---
+++ API/RemeTask/RemeTask/Auth/CustomClaims.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+
+namespace RemeTask.Models
+{
+    public class CustomClaims
+    {
+        public const string UserId = "userId";
+        public const string ClientId = "clientId";
+    }
+}
