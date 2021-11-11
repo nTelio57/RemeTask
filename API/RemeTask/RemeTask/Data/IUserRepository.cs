3b6798a5720679c61e38478587493cc04bf33567
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-11 18:17:35 +0200

    Remove redundant user functions

--- API/RemeTask/RemeTask/Data/IUserRepository.cs
+++ API/RemeTask/RemeTask/Data/IUserRepository.cs
@@ -10,12 +10,7 @@ namespace RemeTask.Data
     public interface IUserRepository
     {
         Task<bool> SaveChanges();
-        Task<User> GetUserByEmail(string email);
         Task<string> GenerateToken(string email);
-        Task AddNewUser(User user);
-        Task<User> GetUserByLogin(string email, string password);
-        Task<User> GetUserById(string id);
-        Task<IEnumerable<User>> GetUsersByEmail(string email);
         Task UpdateUser(User user);
     }
 }
