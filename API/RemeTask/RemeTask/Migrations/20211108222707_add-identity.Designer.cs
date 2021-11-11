01bc81eb77a661ea7bc0e00522edaf024144a2d3
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-11-11 17:53:06 +0200

    Update migrations

--- API/RemeTask/RemeTask/Migrations/20211108222707_add-identity.Designer.cs
+++ API/RemeTask/RemeTask/Migrations/20211108222707_add-identity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RemeTask.Data;
 
 namespace RemeTask.Migrations
 {
     [DbContext(typeof(RemetaskContext))]
-    partial class RemetaskContextModelSnapshot : ModelSnapshot
+    [Migration("20211108222707_add-identity")]
+    partial class addidentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
