2cfbd75d5689f5af88dbac0c7513ddc2f85dc7f8
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-12-07 12:10:28 +0200

    Add custom claim for roles

--- API/RemeTask/RemeTask/Auth/CustomClaims.cs
+++ API/RemeTask/RemeTask/Auth/CustomClaims.cs
@@ -10,5 +10,6 @@ namespace RemeTask.Models
     {
         public const string UserId = "userId";
         public const string ClientId = "clientId";
+        public const string UserRoles = "userRoles";
     }
 }
